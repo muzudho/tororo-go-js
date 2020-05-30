2271a3946a528ef2da4efc5944dacccbeda46e75
むずでょ <muzudho1@gmail.com>
2020-05-31 06:45:16 +0900

    Stone density.

--- tool/make_table_tororo3.py
+++ tool/make_table_tororo3.py
@@ -1,5 +1,5 @@
 from pytororo.pytororo import resize13to18str, line_to_table_str, line_to_table, stone_board, black_stone, white_stone, sticky_rice_cake, sticky_rice_cake_board, print_stone_board, print_cake_board, print_cake_cover, convolute
-from pytororo.search import stone_density_node, stone_density_sq
+from pytororo.search import stone_density_node, stone_density_sq, directed_stone_density_node, directed_stone_density_sq
 
 
 def go():
@@ -22,11 +22,13 @@ oox....xxxoo./
     """
 
     stone_board1 = stone_board(board)
+
     # 18x18=324
     stone_board324 = resize13to18str(stone_board1, '.')
     stone_board18x18 = line_to_table_str(stone_board324, 18)
     print(stone_board18x18)
 
+    print('Trace   | 石密度node。')
     density_node_board324 = stone_density_node(stone_board324)
     # print(density_node_board324)
     density_node_board18x18 = line_to_table(density_node_board324, 18)
@@ -36,11 +38,41 @@ oox....xxxoo./
     for row in density_node_board18x18:
         print(row_format.format(*row))
 
+    print('Trace   | 石密度sq。')
     density_sq_board324 = stone_density_sq(stone_board324)
     density_sq_board18x18 = line_to_table(density_sq_board324, 18)
 
     row_format = "{:>6.02f}" * 18
     for row in density_sq_board18x18:
+        print(row_format.format(*row))
+
+    ccw90 = 0
+    print('Trace   | 東向き石密度node。')
+    d_density_node_board324 = directed_stone_density_node(
+        stone_board324, ccw90)
+    d_density_node_board18x18 = line_to_table(d_density_node_board324, 18)
+
+    row_format = "{:>6.02f}" * 18
+    for row in d_density_node_board18x18:
+        print(row_format.format(*row))
+
+    print('Trace   | 東向き石密度sq。')
+    d_density_sq_board324 = directed_stone_density_sq(
+        stone_board324, ccw90)
+    d_density_sq_board18x18 = line_to_table(d_density_sq_board324, 18)
+
+    row_format = "{:>6.02f}" * 18
+    for row in d_density_sq_board18x18:
+        print(row_format.format(*row))
+
+    print('Trace   | 北東向き石密度。')
+    ccw90 = 1
+    d_density_node_board324 = directed_stone_density_node(
+        stone_board324, ccw90)
+    d_density_node_board18x18 = line_to_table(d_density_node_board324, 18)
+
+    row_format = "{:>6.02f}" * 18
+    for row in d_density_node_board18x18:
         print(row_format.format(*row))
 
     print('Trace   | Finished.')
--- tool/pytororo/search.py
+++ tool/pytororo/search.py
@@ -17,7 +17,16 @@ for y in range(-2, 4):
         window_6x6.append((x, y))
 # print(f'window_6x6=len:{len(window_6x6)} {window_6x6}')
 
+
+def ccw90(p):
+    """反時計回りに90°回転"""
+    return (-p[1], p[0])
+
+
+dir_node_map = []
+
 """
+西向きを手動で作ります。
 -2 . . x
 -1 . x x
  0 @ x x
@@ -25,10 +34,10 @@ for y in range(-2, 4):
  2 . . x
    0 1 2
 """
-eastward = [(2, -2), (1, -1), (2, -1), (1, 0),
-            (2, 0), (1, 1), (2, 1), (2, 2)]
-
+dir_node_map.append([(2, -2), (1, -1), (2, -1), (1, 0),
+                     (2, 0), (1, 1), (2, 1), (2, 2)])
 """
+北西を手動で作ります。
 -2 x x x
 -1 x x x
  0 @ x x
@@ -36,22 +45,44 @@ eastward = [(2, -2), (1, -1), (2, -1), (1, 0),
  2 . . .
    0 1 2
 """
-north_eastward = [(0, -2), (1, -2), (2, -2), (0, -1),
-                  (1, -1), (2, -1), (1, 0), (2, 0)]
+dir_node_map.append([(0, -2), (1, -2), (2, -2), (0, -1),
+                     (1, -1), (2, -1), (1, 0), (2, 0)])
 
-"""
--2  x  x  x  x  x
--1  x  x  x  x  x
- 0  x  x  @  x  x
- 1  x  x  x  x  x
- 2  x  x  x  x  x
-   -2 -1  0  1  2
-"""
-neighbor24_map = [(-2, -2), (-1, -2), (0, -2), (1, -2), (2, -2),
-                  (-2, -1), (-1, -1), (0, -1), (1, -1), (2, -1),
-                  (-2, 0), (-1, 0), (1, 0), (2, 0),
-                  (-2, 1), (-1, 1), (0, 1), (1, 1), (2, 1),
-                  (-2, 2), (-1, 2), (0, 2), (1, 2), (2, 2)]
+# あとは90°回転で作るぜ☆（＾～＾）
+dir_node_map.append(list(map(lambda p: ccw90(p), dir_node_map[0])))
+dir_node_map.append(list(map(lambda p: ccw90(p), dir_node_map[1])))
+dir_node_map.append(list(map(lambda p: ccw90(p), dir_node_map[2])))
+dir_node_map.append(list(map(lambda p: ccw90(p), dir_node_map[3])))
+dir_node_map.append(list(map(lambda p: ccw90(p), dir_node_map[4])))
+dir_node_map.append(list(map(lambda p: ccw90(p), dir_node_map[5])))
+# print(f'dir_node_map=len:{len(dir_node_map)} {dir_node_map}')
+
+
+def drop_shadow(p):
+    """右下に影が落ちるような座標"""
+    return p, (p[0]+1, p[1]), (p[0], p[1]+1), (p[0]+1, p[1]+1)
+
+
+def flat(m):
+    """重複とネストを解消します。順はばらばらになります。"""
+    s = set()
+    for items in m:
+        # items は ((),(),(),()) みたいに丸かっこがネストしている。
+        for item in items:
+            s.add(item)
+    return list(s)
+
+
+dir_sq_map = []
+dir_sq_map.append(flat(map(lambda p: drop_shadow(p), dir_node_map[0])))
+dir_sq_map.append(flat(map(lambda p: drop_shadow(p), dir_node_map[1])))
+dir_sq_map.append(flat(map(lambda p: drop_shadow(p), dir_node_map[2])))
+dir_sq_map.append(flat(map(lambda p: drop_shadow(p), dir_node_map[3])))
+dir_sq_map.append(flat(map(lambda p: drop_shadow(p), dir_node_map[4])))
+dir_sq_map.append(flat(map(lambda p: drop_shadow(p), dir_node_map[5])))
+dir_sq_map.append(flat(map(lambda p: drop_shadow(p), dir_node_map[6])))
+dir_sq_map.append(flat(map(lambda p: drop_shadow(p), dir_node_map[7])))
+# print(f'dir_sq_map=len:{len(dir_sq_map)} {dir_sq_map}')
 
 
 def stone_density_node(stone_board):
@@ -85,6 +116,61 @@ def stone_density_sq(stone_board):
     return num_board
 
 
+def directed_stone_density_node(stone_board, ccw45: int):
+    """
+    Parameters
+    ----------
+    ccw45:
+        0 - 東
+        1 - 北東
+        2 - 北
+        3 - 北西
+        4 - 西
+        5 - 南西
+        6 - 南
+        7 - 南東
+    """
+    num_board = [0] * (18*18)
+
+    def count_up(stone_board, p, pp):
+        nonlocal num_board
+        if stone_board[to_addr18x18(pp)] != '.':
+            num_board[to_addr18x18(p)] += 1
+
+    scan_pure_board(lambda p: scan_window_dir_node(
+        lambda pp: count_up(stone_board, p, pp), ccw45, p))
+    return num_board
+
+
+def directed_stone_density_sq(stone_board, ccw45: int):
+    """
+    Parameters
+    ----------
+    ccw45:
+        0 - 東
+        1 - 北東
+        2 - 北
+        3 - 北西
+        4 - 西
+        5 - 南西
+        6 - 南
+        7 - 南東
+    """
+    num_board = [0] * (18*18)
+
+    def count_up(stone_board, p, pp):
+        nonlocal num_board
+        if stone_board[to_addr18x18(pp)] != '.':
+            num_board[to_addr18x18(p)] += 0.25
+            num_board[to_addr18x18((p[0]+1, p[1]))] += 0.25
+            num_board[to_addr18x18((p[0], p[1]+1))] += 0.25
+            num_board[to_addr18x18((p[0]+1, p[1]+1))] += 0.25
+
+    scan_pure_board(lambda p: scan_window_dir_sq(
+        lambda pp: count_up(stone_board, p, pp), ccw45, p))
+    return num_board
+
+
 def scan_pure_board(f):
     """枠を除いた、盤上を１マスずつイテレートして座標を返します。Y座標は行番号です。"""
     for p in pure_board_map:
@@ -103,29 +189,16 @@ def scan_window_6x6(f, p):
         f((p[0]+pp[0], p[1]+pp[1]))
 
 
-def directed_stone_density_node():
-    pass
+def scan_window_dir_node(f, ccw45, p):
+    """起点を p とする方向性のあるウィンドウの各マスをイテレートして座標を返します。"""
+    for pp in dir_node_map[ccw45]:
+        f((p[0]+pp[0], p[1]+pp[1]))
 
 
-def directed_stone_density_sq():
-    pass
-
-
-def ccw90(p):
-    """反時計回りに90°回転"""
-    return (-p[1], p[0])
-
-
-def ccw180(p):
-    """反時計回りに180°回転"""
-    p = ccw90((-p[1], p[0]))
-    return (-p[1], p[0])
-
-
-def ccw270(p):
-    """反時計回りに270°回転"""
-    p = ccw180((-p[1], p[0]))
-    return (-p[1], p[0])
+def scan_window_dir_sq(f, ccw45, p):
+    """起点を p とする方向性のあるウィンドウの各マスをイテレートして座標を返します。"""
+    for pp in dir_sq_map[ccw45]:
+        f((p[0]+pp[0], p[1]+pp[1]))
 
 
 def to_addr18x18(p):
